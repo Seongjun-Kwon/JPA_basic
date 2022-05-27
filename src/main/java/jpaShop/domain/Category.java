c111a9cc12212cdd81f33525282e7b591d345c20
Kwon seong jun <tjdwns1865@naver.com>
2022-05-27 22:09:54 +0900

    상속관계 매핑 1. 상품의 종류로 음반, 도서, 영화가 있고 이후 더 확장될 수 있다고 가정한다. 2. 모든 데이터에 등록일과 수정일을 필수로 넣는다.

--- src/main/java/jpaShop/domain/Category.java
+++ src/main/java/jpaShop/domain/Category.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Category {
+public class Category extends BaseEntity {
 
     @Id
     @GeneratedValue
@@ -14,16 +14,16 @@ public class Category {
     private String name;
 
     @ManyToOne
-    @JoinColumn(name="PARENT_ID")
+    @JoinColumn(name = "PARENT_ID")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
 
     @ManyToMany
-    @JoinTable(name="CATEGORY_ITEM",
-            joinColumns = @JoinColumn(name="CATEGORY_ID"),
-            inverseJoinColumns = @JoinColumn(name="ITEM_ID")
+    @JoinTable(name = "CATEGORY_ITEM",
+            joinColumns = @JoinColumn(name = "CATEGORY_ID"),
+            inverseJoinColumns = @JoinColumn(name = "ITEM_ID")
     )
     private List<Item> items = new ArrayList<>();
 
